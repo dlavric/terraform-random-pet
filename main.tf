d5027e6e664118ad8c9211d652f7e6ad2ce884e9
Daniela Lavric <daniela@hashicorp.com>
2022-08-04 14:44:28 +0200

    added remote backend for TFE

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ resource "random_pet" "server" {
 output "server" {
   value = random_pet.server.id
 }
+
+terraform {
+  backend "remote" {
+    hostname = "192.168.56.33.nip.io"
+    organization = "skills-map"
+
+    workspaces {
+      name = "random-pet"
+    }
+  }
+}
