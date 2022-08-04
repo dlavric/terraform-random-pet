cc86090e5a6a9d12ea3b987bfa9aaed14f43e44b
Daniela Lavric <daniela@hashicorp.com>
2022-08-04 16:08:14 +0200

    added the data source terraform_remote_state

--- main.tf
+++ main.tf
@@ -1,23 +1,25 @@
-provider "random" {
-  # Configuration options
-}
-
-resource "random_pet" "server" {
-  length = 3
-
-}
-
-output "server" {
-  value = random_pet.server.id
-}
-
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
     organization = "daniela-org"
 
     workspaces {
+      name = "remote-random-pet"
+    }
+  }
+}
+
+data "terraform_remote_state" "random-pet" {
+  backend = "remote"
+
+  config = {
+    organization = "daniela-org"
+    workspaces = {
       name = "random-pet"
     }
   }
+}
+
+output "from_other" {
+value = data.terraform_remote_state.random-pet.outputs.server
 }
